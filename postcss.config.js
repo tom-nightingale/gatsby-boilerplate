0d598a6d7546b68d17c44a6f492c3336d477ab9b
Tom Nightingale <thomas.nightingale@gmail.com>
2019-07-16 23:24:23 +0100

    Removed gatsby-browser (not needed rn) Updated post-css to

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,12 @@
 // postcss.config.js
 const purgecss = require('@fullhuman/postcss-purgecss')({
 
-  // Specify the paths to all of the template files in your project 
+  // Specify the paths to all of the template files in your project
   content: [
     './src/**/*.html',
     './src/**/*.vue',
     './src/**/*.jsx',
+    './src/**/*.js',
     // etc.
   ],
 
