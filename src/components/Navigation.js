01d3c4c6b1d93bebf34bdedafd07de4d503f9aaa
Tom Nightingale <thomas.nightingale@gmail.com>
2019-07-16 22:38:43 +0100

    Nav and subnav working well

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,13 @@
 import React from "react"
 import { Link } from "gatsby";
 
+import LeagueLogoText from "../svg/league-logo-text.svg";
+import LeagueBgImage from "../img/league-bg.jpg";
+
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosMenu } from "react-icons/io";
 import { IoMdClose } from "react-icons/io";
+import { IoIosMailUnread } from "react-icons/io";
 
 const Navigation = class extends React.Component {
     constructor(props) {
@@ -50,9 +54,27 @@ const Navigation = class extends React.Component {
 
               </nav>
 
-              <nav className={`w-full bg-gray-300 absolute top-0 left-0 p-10 min-h-screen z-10 ${this.state.navBarActiveClass}`}>
-                  <a className="absolute top-0 right-0 p-10 text-4xl" href="#" onClick={() => this.toggleHamburger()}><IoMdClose /></a>
-              </nav>
+              <div className={`w-1/4 bg-white absolute top-0 right-0 p-10 pt-16 min-h-screen z-10 ${this.state.navBarActiveClass}`}>
+
+                  <a className="absolute top-0 right-0 p-10 mt-2 pt-12 text-4xl text-navy-500" href="#" onClick={() => this.toggleHamburger()}><IoMdClose /></a>
+
+                  <nav className="flex flex-col justify-center pl-10 text-navy-500 font-MRextrabold ">
+                      <Link to="/"><LeagueLogoText className="mt-4 mb-8 w-32 mb-8" /></Link>
+                      <Link to="/" className="py-3">our services</Link>
+                      <Link to="/" className="py-3 pl-4 text-teal-500">web design</Link>
+                      <Link to="/" className="py-3 pl-4 text-teal-500">web development</Link>
+                      <Link to="/" className="py-3 pl-4 text-teal-500">seo</Link>
+                      <Link to="/" className="py-3 pl-4 text-teal-500">ppc</Link>
+                      <Link to="/" className="py-3 pl-4 text-teal-500">content marketing</Link>
+                      <Link to="/" className="py-3 pl-4 text-teal-500">copywriting</Link>
+                      <Link to="/" className="py-3 pl-4 text-teal-500">telecoms</Link>
+                      <Link to="/" className="py-3">about us</Link>
+                      <Link to="/" className="py-3">our work</Link>
+                      <Link to="/" className="py-3">latest news</Link>
+                      <Link to="/" className="flex flex-wrap items-center py-3"><IoIosMailUnread className="text-2xl mr-2"/> work with us</Link>
+                  </nav>
+
+              </div>
 
           </div>
       )
