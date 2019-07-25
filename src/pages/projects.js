c91d7de1a218dff68f7494b98f206a109178bb0c
Tom Nightingale <thomas.nightingale@gmail.com>
2019-07-25 21:51:48 +0100

    Pulling projects from a Dataset (less manual) Updated styling to reflect better page layout

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -16,9 +16,48 @@ import LeagueLogoText from "../svg/league-logo-text.svg";
 import LeagueBgImage from "../img/league-bg.jpg";
 
 
-const IndexPage = (props) => {
+const ProjectsPage = (props) => {
 
+    //Declare projects dataset
     console.log(props);
+    console.log(props.data.leagueBg.childImageSharp.fluid)
+    const projects = [
+        {
+            title: "Carlton Town Football Club",
+            bgColor: '#000',
+            coverImage: "carlton-town-bg",
+            services: ["Web Design & Development"],
+            url: "/",
+        },
+        {
+            title: "Safe Haven Day Nursery",
+            bgColor: 'red',
+            coverImage: "safe-haven-bg",
+            services: ["Web Design & Development"],
+            url: "/",
+        },
+        {
+            title: "Esland Care",
+            bgColor: 'blue',
+            coverImage: "esland-care-bg",
+            services: ["Web Design & Development"],
+            url: "/",
+        },
+        {
+            title: "Midland Speed Champs",
+            bgColor: 'green',
+            coverImage: "msc-bg",
+            services: ["Web Development"],
+            url: "/",
+        },
+        {
+            title: "Trust Precision Engineering",
+            bgColor: 'yellow',
+            coverImage: "league-bg",
+            services: ["Web Design & Development"],
+            url: "/",
+        }
+    ];
 
     return (
 
@@ -26,7 +65,7 @@ const IndexPage = (props) => {
 
             <div>
 
-                <div className="relative container max-w-full md:flex md:flex-wrap md:justify-between mx-auto overflow-hidden">
+                <div className="relative container max-w-full md:flex md:flex-wrap md:justify-between mx-auto">
 
                     <div className="md:w-1/2 lg:w-2/12 p-10 py-16 md:p-16 lg:pl-20 flex flex-wrap flex-col bg-navy-600 text-beige-100">
 
@@ -49,62 +88,43 @@ const IndexPage = (props) => {
 
                 </div>
 
-                <div className="container max-w-full w-11/12 flex flex-wrap mx-auto">
+                <div className="container max-w-full w-11/12 flex flex-wrap mx-auto py-4 sm:py-8 md:py-12 lg:py-16">
 
-                    <Link to="/" className="w-100 sm:w-1/2 lg:w-1/3 p-4 bg-beige-100">
+                {
+                    projects.map(function(project) {
 
-                        <div className="relative group overflow-hidden w-100 bg-white">
+                        var services = project.services.map(function(service) {
+                            return(
+                                <li key={service}>{service}</li>
+                            )
+                        });
 
-                            <div className="transition-all transition-250">
-                                <h2 className="text-teal-500">title</h2>
-                                <p>qLorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam facilis veritatis obcaecati doloribus labore neque, repellendus est, explicabo deleniti, tempora, nam assumenda sint aperiam! Qui cum dolores labore nihil est!</p>
-                            </div>
+                        return (
+                            <AniLink
+                            key={project.title}
+                            paintDrip
+                            duration={.75}
+                            hex="#0b1529"
+                            to={project.url}
+                            className="w-full sm:w-1/2 lg:w-1/3 p-4 md:p-8 mb-4"
+                            >
 
-                        </div>
+                                <div className="relative group overflow-hidden w-full bg-white min-h-64 lg:min-h-128 bg-gray-100">
 
-                    </Link>
-                    <Link to="/" className="w-100 sm:w-1/2 lg:w-1/3 p-4 bg-beige-100">
+                                    <div className="transition-all transition-250">
+                                        <h2 className="text-teal-500">{project.title}</h2>
+                                        <p>{project.coverImage}</p>
+                                        <ul>
+                                            {services}
+                                        </ul>
+                                    </div>
 
-                        <div className="relative group overflow-hidden w-100 bg-white">
+                                </div>
 
-                            <div className="transition-all transition-250">
-                                <h2 className="text-teal-500">title</h2>
-                                <p>qLorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam facilis veritatis obcaecati doloribus labore neque, repellendus est, explicabo deleniti, tempora, nam assumenda sint aperiam! Qui cum dolores labore nihil est!</p>
-                            </div>
-
-                        </div>
-
-                    </Link>
-
-                    <Link to="/" className="w-100 sm:w-1/2 lg:w-1/3 p-4 bg-beige-100">
-
-                        <div className="relative group overflow-hidden w-100 bg-white">
-
-                            <div className="transition-all transition-250">
-                                <h2 className="text-teal-500">title</h2>
-                                <p>qLorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam facilis veritatis obcaecati doloribus labore neque, repellendus est, explicabo deleniti, tempora, nam assumenda sint aperiam! Qui cum dolores labore nihil est!</p>
-                            </div>
-
-
-
-                        </div>
-
-                    </Link>
-
-                    <Link to="/" className="w-100 sm:w-1/2 lg:w-1/3 p-4 bg-beige-100">
-
-                        <div className="relative group overflow-hidden w-100 bg-white">
-
-                            <div className="transition-all transition-250">
-                                <h2 className="text-teal-500">title</h2>
-                                <p>qLorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam facilis veritatis obcaecati doloribus labore neque, repellendus est, explicabo deleniti, tempora, nam assumenda sint aperiam! Qui cum dolores labore nihil est!</p>
-                            </div>
-
-
-
-                        </div>
-
-                    </Link>
+                            </AniLink>
+                        )
+                    })
+                }
 
                 </div>
 
@@ -114,7 +134,7 @@ const IndexPage = (props) => {
     );
 }
 
-export default IndexPage;
+export default ProjectsPage;
 
 export const pageQuery = graphql`
   query {
