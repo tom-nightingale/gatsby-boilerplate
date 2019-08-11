4bed5371d37a2b09104914b66375bd7cce0f399d
Tom Nightingale <thomas.nightingale@gmail.com>
2019-08-11 21:15:26 +0100

    Amending custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,11 +4,11 @@ import { graphql } from "gatsby"
 import BackgroundImage from 'gatsby-background-image'
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 
-import PageHeader from "../../components/PageHeader"
-import ContactForm from "../../components/ContactForm"
-import NavigationHolder from "../../components/nav/NavigationHolder"
+import PageHeader from "../components/PageHeader"
+import ContactForm from "../components/ContactForm"
+import NavigationHolder from "../components/nav/NavigationHolder"
 
-import LeagueIconBeige from "../../svg/league-icon-beige.svg"
+import LeagueIconBeige from "../svg/league-icon-beige.svg"
 
 
 const NotFoundPage = (props) => {
@@ -27,16 +27,16 @@ const NotFoundPage = (props) => {
                 </BackgroundImage>
 
                     <div className="p-8 md:p-12 lg:p-24 xl:p-32 md:w-2/3 xl:w-1/2">
-                        <h2 className="text-teal-500 md:text-teal-300 text-lg md:text-4xl opacity-50">Latest Projects</h2>
-                        <p className="text-3xl lg:text-5xl leading-tight mt-2 mb-4 md:mb-8 text-navy-600 font-MRextrabold">Midland Speed Championships</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, porro. Alias iste ex, quas nisi temporibus adipisci fugiat voluptates vel hic blanditiis, sunt illo eligendi? Voluptatibus sunt suscipit ut fugiat.</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, expedita blanditiis odit maxime. Laudantium veniam est, temporibus quas aperiam rerum tempora deleniti, eaque cum quidem quisquam quam, culpa iusto ad.</p>
+                        <h2 className="text-teal-500 md:text-teal-300 text-lg md:text-4xl opacity-50">404 Error</h2>
+                        <p className="text-3xl lg:text-5xl leading-tight mt-2 mb-4 md:mb-8 text-navy-600 font-MRextrabold">Page not found!</p>
+                        <p>Unfortunately, it looks like the page you requeted doesn't exist any more.</p>
+                        <p>Try visiting one of the pages in our navigation, or try heading to the homepage.</p>
                         <AniLink
-                            to="/projects/midland-speed-champs"
+                            to="/"
                             className="link-underline link-teal"
                             paintDrip
                             duration={.75}
-                            hex="#1BB6BA">View project
+                            hex="#1BB6BA"> Go home
                         </AniLink>
                     </div>
 
