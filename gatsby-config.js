bc5dd14dbf74ce9963fd569419f4ff4248cab5f4
Tom Nightingale <thomas.nightingale@gmail.com>
2019-10-15 23:04:39 +0100

    replacing UA code

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "GA-UA-150195377-1", // Google Analytics / GA
+          "UA-150195377-1", // Google Analytics / GA
         ],
         
         // This object is used for configuration specific to this plugin
