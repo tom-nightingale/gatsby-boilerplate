603f39c6ee1ddd288567b3e6a0047a0d69f7422b
Tom Nightingale <thomas.nightingale@gmail.com>
2019-10-15 22:32:53 +0100

    adding google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,21 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-transition-link`
+    `gatsby-plugin-transition-link`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-150195377-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: false,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+      },
+    },
   ],
 
 }
